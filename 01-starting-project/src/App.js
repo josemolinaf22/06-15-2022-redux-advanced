b1e366fa79bdc798b03d02a32bec84aa76624db5
Jose Molina <josemolinaf22@gmail.com>
2022-06-16 06:21:28 -0600

    finished

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,11 +1,51 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useEffect } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector, useDispatch } from "react-redux";
+import {uiActions} from './store/ui-slice'
 
 function App() {
+  const dispatch = useDispatch()
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      )
+      const response = await fetch(
+        'https://react-http.firebaseio.com'
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+        if(!response.ok){
+          throw new Error('sending cart data falied')
+        }
+
+        dispatch(
+          uiActions.showNotification({
+            status: 'success',
+            title: 'Success!',
+            message: 'Sent cart data sucessfully',
+          })
+        
+          
+          )
+        
+    })
+  }, [cart]);
   return (
     <Layout>
-      <Cart />
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
